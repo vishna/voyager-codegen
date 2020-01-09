4cab5b925f5b03717964d7a359791ee91609ed4c
Lukasz Wisniewski <luke.cherrish@gmail.com>
2020-01-09 13:03:43 +0100

    use extensions for voyager strongly typed fields

--- voyager-codegen/src/test/resources/test_3.dart
+++ voyager-codegen/src/test/resources/test_3.dart
@@ -25,10 +25,7 @@ String pathSuper({String await_, String async_}) {
 
 const String typeSuper = "super";
 
-class VoyagerData extends Voyager {
-  VoyagerData({String path, Voyager parent, Map<String, dynamic> config})
-      : super(path: path, parent: parent, config: config);
-
+extension VoyagerData on Voyager {
   String get title => this["title"];
   String get body => this["body"];
   String get fabPath => this["fabPath"];
@@ -38,17 +35,6 @@ class VoyagerData extends Voyager {
   List<dynamic> get actions => this["actions"];
   List<dynamic> get items => this["items"];
 }
-
-class VoyagerProvider {
-  static VoyagerData of(BuildContext context) => Provider.of<Voyager>(context);
-}
-
-VoyagerData voyagerDataFactory(
-        AbstractRouteContext abstractContext, Map<String, dynamic> config) =>
-    VoyagerData(
-        path: abstractContext.url(),
-        parent: abstractContext.getExtras().parent,
-        config: config);
 
 abstract class IconPluginStub extends RouterObjectPlugin<Icon> {
   IconPluginStub() : super("icon");
